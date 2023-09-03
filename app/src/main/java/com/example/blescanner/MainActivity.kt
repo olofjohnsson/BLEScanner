65a478fffa3af3ada130c9bf0e6a2cb60bb02b61
olofjohnsson <olof.s.johnsson@gmail.com>
2023-09-03 19:04:40 +0200

    Initial commit

---
+++ app/src/main/java/com/example/blescanner/MainActivity.kt
@@ -0,0 +1,165 @@
+package com.example.blescanner
+
+import android.Manifest
+import android.Manifest.permission.BLUETOOTH_SCAN
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanFilter
+import android.bluetooth.le.ScanResult
+import android.bluetooth.le.ScanSettings
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
+import com.example.blescanner.ui.theme.BleScannerTheme
+import kotlin.reflect.KProperty
+
+class MainActivity : ComponentActivity() {
+
+    companion object{
+        private val TAG = "OLLEBTSCANNER"
+        val BLUETOOTH_REQUEST_CODE = 1
+
+    }
+
+    private val bluetoothAdapter: BluetoothAdapter by lazy {
+        (getSystemService(BLUETOOTH_SERVICE) as BluetoothManager).adapter
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BleScannerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        if (bluetoothAdapter.isEnabled){
+            // start scanning
+            startBLEScan()
+        }
+        else
+        {
+            Log.v(TAG, "BT is disabled")
+            val btIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.BLUETOOTH_CONNECT
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return
+            }
+            startActivityForResult(btIntent, BLUETOOTH_REQUEST_CODE)
+        }
+    }
+
+    private fun startBLEScan()
+    {
+        Log.v(TAG, "Start BLEScan")
+
+        val byteArray = ByteArray(2)
+
+        val manufacturerId = 0x0707 // Replace with the desired manufacturer ID
+        val manufacturerData = byteArrayOf(0x00, 0x07, 0x00, 0x07)
+
+        val scanFilter = ScanFilter.Builder()
+            //.setDeviceAddress("F3:33:30:F8:63:C8")
+            //.setDeviceName("B4E55336235030AEE9")
+            //.setManufacturerData(manufacturerId, manufacturerData)
+            .build()
+
+        val scanFilters:MutableList<ScanFilter> = mutableListOf()
+        scanFilters.add(scanFilter)
+
+        val scanSettings = ScanSettings.Builder()
+            .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
+            .build()
+
+        Log.v(TAG, "Start scan...")
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.BLUETOOTH_SCAN
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            Log.v(TAG, "missing Scan permission...")
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.BLUETOOTH_SCAN),
+                BLUETOOTH_REQUEST_CODE
+            )
+            return
+        }
+        bluetoothAdapter.bluetoothLeScanner.startScan(scanFilters, scanSettings, bleScanCallback)
+    }
+
+    private val bleScanCallback : ScanCallback by lazy {
+        object : ScanCallback(){
+            override fun onScanResult(callbackType: Int, result: ScanResult?) {
+                //super.onScanResult(callbackType, result)
+                Log.v(TAG, "onScanResult")
+
+                val bluetoothDevice = result?.device
+                val bluetoothData = result?.scanRecord
+                if (bluetoothDevice != null)
+                {
+                    if (ActivityCompat.checkSelfPermission(
+                            this@MainActivity,
+                            Manifest.permission.BLUETOOTH_CONNECT
+                        ) != PackageManager.PERMISSION_GRANTED
+                    ) {
+                        Log.v(TAG, "Need permission BLUETOOTH_CONNECT")
+                        return
+                    }
+                    if (bluetoothData != null) {
+                        Log.v(TAG, "Device Name ${bluetoothDevice.name} Device UUIDS ${bluetoothDevice.uuids} Device Address ${bluetoothDevice.address} Record ${bluetoothData.manufacturerSpecificData}")
+                    }
+                }
+
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BleScannerTheme {
+        Greeting("Android")
+    }
+}
